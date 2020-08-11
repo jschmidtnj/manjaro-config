7f0535aa1e4a07b77e2e750880ab47cb394c6c84
Joshua Schmidt <jschmidtnj@gmail.com>
2020-08-11 16:10:53 -0400

    fixed install script
    Signed-off-by: Joshua Schmidt <jschmidtnj@gmail.com>

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ yay -Syu --needed visual-studio-code-bin icaclient google-chrome vlc \
   slack-desktop jdk8-openjdk cloc dos2unix baobab postman insomnia \
   xdotool wmctrl libinput-gestures noto-fonts-emoji aws-cli zoom surfshark-vpn \
   namcap gitkraken balena-etcher tigervnc chromium krdc unzip \
-  foldingathome
+  foldingathome plex-media-server
 
 # install boinc (seti)
 yay -Syu --needed boinc
