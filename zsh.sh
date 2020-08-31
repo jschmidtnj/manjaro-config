1b39356ae6496c8ee1ad76c1d3f3e8962fd89821
Joshua Schmidt <jschmidtnj@gmail.com>
2020-08-31 19:05:05 -0400

    config: added zsh config files
    Signed-off-by: Joshua Schmidt <jschmidtnj@gmail.com>

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
+# yay -Syu --needed nerd-fonts-complete
+
 # install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # get power level 10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
