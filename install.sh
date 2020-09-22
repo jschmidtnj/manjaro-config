f179f2070f760b3ec91f314c1ee6852e8e811da3
Joshua <jschmidtnj@gmail.com>
2020-09-22 16:32:07 -0400

    manjaro: fixed install user
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ if ! [ -x "$(command -v yay)" ] || ! [ -d /opt/yay-git ]; then
   cd /opt
   sudo rm -rf yay-git
   sudo git clone https://aur.archlinux.org/yay-git.git
-  sudo chown -R joshua:joshua ./yay-git
+  user=$(whoami)
+  sudo chown -R $user:$user ./yay-git
   cd yay-git
   makepkg -si
 fi
