aa5d473ae0f33c3ba61398219792d469cb9a60d2
Joshua <jschmidtnj@gmail.com>
2020-12-14 23:13:22 -0500

    plex: added sleeps for updates
    Signed-off-by: Joshua <jschmidtnj@gmail.com>

--- update.sh
+++ update.sh
@@ -4,7 +4,12 @@ set -e
 
 yay -Syu
 
+sleep 5 # sleep for a few seconds before running the rest
+
+echo "updating plex with new settings"
+
 sudo sed -i 's/=plex/=joshua/' /usr/lib/systemd/system/plexmediaserver.service
+sleep 1
 sudo systemctl daemon-reload
 sudo systemctl restart plexmediaserver
 
